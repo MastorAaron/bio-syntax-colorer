3794b6af01264910a56dfbe2c4a20480466c738c
Rieser, Aaron <aaronrieser@my.unt.edu>
2025-06-21 04:41:55 -0500

    FINALLY Working Dynamic Build v17

--- extension.js
+++ extension.js
@@ -1,8 +1,34 @@
 const vscode = require("vscode");
 const { patchTokenColors } = require("./patch");
 
-function activate(context){
-    patchTokenColors(context);
+async function isActive(){
+    const config = vscode.workspace.getConfiguration();
+    // const current = config.get("editor.tokenCustomizations") || {};
+    // const currRULES = current.textMateRules || [];
+    
+    // return currRULES.some(rule =>
+    //     typeof rule.comment === "string" && rule.comment.startsWith("bio-colorer@")
+    // );
+    return config.get("bioNotation.enabled");
+}
+
+async function activate(context){
+    const toggleCommand = vscode.commands.registerCommand("bioNotation.toggleColors", async () =>{
+        if(await isActive()){
+            await removeTokenColors() 
+            vscode.window.showInformationMessage("BioNotation colors Deactivated.");
+        }else{
+            await patchTokenColors(context);
+            vscode.window.showInformationMessage("BioNotation colors Activated.");
+        }
+    });
+
+    context.subscriptions.push(toggleCommand);
+
+    if (!await isActive()) {
+        await patchTokenColors();
+        console.log("BioNotation Extension Activated.");
+    }
 }
 
 // function deactivate() {
@@ -17,7 +43,7 @@ async function deactivate() {
     const cleanedRules = Array.isArray(current.textMateRules)
     ? current.textMateRules.filter(rule => {
         const comment = rule.comment || "";
-        return !comment.startsWith("bio-syntax-colorer@");
+        return !comment.startsWith("bio-colorer@");
       })
     : [];
 
--- patch.js
+++ patch.js
@@ -1,41 +1,34 @@
 const vscode = require("vscode");
-const fs = require("fs");
 const path = require("path");
-const { version } = require("./package.json");
+const fs = require("fs");
 const { version } = require("./package.json");
 
-function patchTokenColors(context) {
-    const tokenColorPath = path.join(context.extensionPath, "themes", "fasta-colors.json");
+async function patchTokenColors(context) {
+    const colorPath = path.join(context.extensionPath, "fasta-colors.json");
 
     try {
-        const data = JSON.parse(fs.readFileSync(tokenColorPath, "utf8"));
-        const rules = data.tokenColors;
+        const data = JSON.parse(fs.readFileSync(colorPath, "utf8"));
+        let rules = data.tokenColors;
 
         // Inject version comment into each rule
          rules = rules.map(rule => ({
             ...rule,
-            comment: `bio-syntax-colorer@${version}`
+            comment: `bio-colorer@${version}`
         }));
 
         const config = vscode.workspace.getConfiguration("editor");
-        const customization = vscode.workspace.getConfiguration("editor").get("tokenColorCustomization") || {};
+        const customization = vscode.workspace.getConfiguration("editor").get("tokenColorCustomizations") || {};
         customization.textMateRules = (customization.textMateRules || []).concat(rules);
 
-        config.update(
-            "tokenColorCustomizations",
-            customization,
-            vscode.ConfigurationTarget.Global
-        );
-
-        vscode.workspace.getConfiguration().update(
+            await vscode.workspace.getConfiguration().update(
             "editor.tokenColorCustomizations",
             customization,
             vscode.ConfigurationTarget.Global
         );
         
-        console.log("BioSyntax patch applied.");
+        console.log("BioNotation patch applied.");
     }catch(err){
-        console.error("Failed to apply Biosyntax patch: ", err)
+        console.error("Failed to apply BioNotation patch: ", err)
     }
 }
 
