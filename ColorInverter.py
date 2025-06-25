526a9d1ec2ea32c693af6c42eb8ac6c6598add20
Rieser, Aaron <aaronrieser@my.unt.edu>
2025-06-25 01:19:04 -0500

    ProtoType of Palette Switcher

--- ColorInverter.py
+++ ColorInverter.py
@@ -2,22 +2,25 @@ import json
 import colorsys
 
 # Load the existing palette
-with open('/mnt/data/fasta-colors.json', 'r') as f:
-    data = json.load(f)
+def loadColorRules():
+    with open('fasta-colors.json', 'r') as inputFile:
+        colorRules = json.load(inputFile)
+    return colorRules
 
 # Helper: invert RGB (negative)
-def invert_hex(hexstr):
-    hexstr = hexstr.lstrip('#')
-    val = int(hexstr, 16)
+def invertHex(hexStr):
+    hexStr = hexStr.lstrip('#')
+    val = int(hexStr, 16)
     inv = 0xFFFFFF - val
     return f'#{inv:06X}'
 
 # Helper: complementary via HSL hue shift (+0.5)
-def complementary_hex(hexstr):
-    hexstr = hexstr.lstrip('#')
-    r = int(hexstr[0:2], 16) / 255.0
-    g = int(hexstr[2:4], 16) / 255.0
-    b = int(hexstr[4:6], 16) / 255.0
+def complementaryHex(hexStr):
+    hexStr = hexStr.lstrip('#')
+    r = int(hexStr[0:2], 16) / 255.0
+    g = int(hexStr[2:4], 16) / 255.0
+    b = int(hexStr[4:6], 16) / 255.0
+    
     # convert RGB to HLS (note: colorsys uses H, L, S)
     h, l, s = colorsys.rgb_to_hls(r, g, b)
     h2 = (h + 0.5) % 1.0
@@ -25,27 +28,27 @@ def complementary_hex(hexstr):
     return '#{:02X}{:02X}{:02X}'.format(int(r2*255), int(g2*255), int(b2*255))
 
 # Build inverted and complementary palettes
-inverted = {'tokenColors': []}
-complementary = {'tokenColors': []}
-
-for rule in data['tokenColors']:
-    new_rule_inv = dict(rule)
-    new_rule_inv['settings'] = dict(rule['settings'])
-    new_rule_inv['settings']['foreground'] = invert_hex(rule['settings']['foreground'])
-    inverted['tokenColors'].append(new_rule_inv)
-
-    new_rule_comp = dict(rule)
-    new_rule_comp['settings'] = dict(rule['settings'])
-    new_rule_comp['settings']['foreground'] = complementary_hex(rule['settings']['foreground'])
-    complementary['tokenColors'].append(new_rule_comp)
+def mapColorRule(colorRules, mappingRule):
+    newColors = {'tokenColors': []}
+    for rule in colorRules['tokenColors']:
+        newRules = dict(rule)
+        newRules['settings'] = dict(rule['settings'])
+        newRules['settings']['foreground'] = mappingRule(rule['settings']['foreground'])
+        newColors['tokenColors'].append(newRules)
+    return newColors
 
 # Save to new JSON files
-inv_path = '/mnt/data/fasta-colors-inverted.json'
-comp_path = '/mnt/data/fasta-colors-complementary.json'
-with open(inv_path, 'w') as f:
-    json.dump(inverted, f, indent=2)
-with open(comp_path, 'w') as f:
-    json.dump(complementary, f, indent=2)
+colorRules = loadColorRules()
+inverted = mapColorRule(colorRules, mappingRule=invertHex)
+complementary = mapColorRule(colorRules, mappingRule=complementaryHex)
+
+inv_path = 'fasta-colors-inverted.json'
+comp_path = 'fasta-colors-complementary.json'
+with open(inv_path, 'w') as outFile:
+    json.dump(inverted, outFile, indent=2)
+    
+with open(comp_path, 'w') as outFile:
+    json.dump(complementary, outFile, indent=2)
 
 # Show a preview of the first three entries from each
 print("Inverted palette sample:")
